7731489fe69b5c2bc353537e5471e2742eb76455
Douglas Fortunato <douglas.fortunato@leadforce.com.br>
2018-05-09 16:35:57 -0300

    Commit inicial do projeto.

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,12 @@
+<?php namespace dex_teste\Http\Controllers;
+
+Class BlogController extends Controller{
+
+    public function index(){
+        return "<h1>Home do blog<h1>";
+    }
+
+    public function post(){
+        
+    }
+}
