2dda9037b5ed49aef7dcdffc3d8d61bc99123875
Douglas Fortunato <douglas.fortunato@leadforce.com.br>
2018-05-12 15:38:24 -0300

    Remoção de duplicação no AdmimController.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -66,40 +66,6 @@ Class AdminController extends Controller{
             ->withInput($request->only(['titulo','id']));
     }
 
-    public function atualizarNoticia(NoticiaRequest $request, Noticias $Noticia){
-
-        $params = $request->all();
-
-        $id = $request->input('id');
-
-        if($id != null){
-            $noticia = Noticias::find($id);
-
-            $noticia->titulo = $request->input('titulo');
-            $noticia->autor = $request->input('autor');
-            $noticia->categoria = $request->input('categoria');
-            $noticia->resumo = $request->input('resumo');
-            $noticia->conteudo = $request->input('conteudo');
-
-            if($noticia->imagem_principal != $request->input('imagem_nome')){
-                $noticia->imagem_principal = $this->uploadImg('imagem_principal');
-            }
-        }else{
-            $noticia = new Noticias($params);
-            $noticia->imagem_principal = $this->uploadImg('imagem_principal');
-        }
-
-        $data = $request->input('data');
-        $noticia->data = \Carbon\Carbon::parse($data)->format('y-m-d');
-        $noticia->slug = str_slug($noticia->titulo);
-
-        $noticia->save();
-
-        return redirect()
-            ->action('AdminController@listaNoticias')
-            ->withInput($request->only(['titulo','id']));
-    }
-
     public function removerNoticia($id){
         $noticia = Noticias::find($id);
         $noticia->delete();
